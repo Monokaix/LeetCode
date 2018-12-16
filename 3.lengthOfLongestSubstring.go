ca69fff6ff81c2b57a49ea2824ef79a86484464f
Monokaix <2536818783@qq.com>
2018-12-16 14:26:04 +0800

    sliding window

--- 3.lengthOfLongestSubstring.go
+++ 3.lengthOfLongestSubstring.go
@@ -1,11 +1,28 @@
 package main
 
-import (
-	"math"
-	"fmt"
-)
+import "fmt"
+
+func lengthOfLongestSubstring(s string) int {
+	freq :=[256]int{}
+	l := 0
+	r :=-1
+	res := 0
+	for l < len(s){
+		if r+1 < len(s) && freq[s[r+1]] == 0{ //判断下一个字符是否已经存在于当前的滑动窗口中 r+1
+			r++
+			freq[s[r]]++
+		}else{
+			freq[s[l]]-- //抛弃左边重复的元素
+			l++ //左边指针向前移动
+		}
+		if res < (r-l+1){
+			res = r-l+1
+		}
+	}
+	return res
+}
 
 func main() {
-	res := math.Max(1,4)
-	fmt.Println(res)
+	s := "abcbbccc"
+	fmt.Println(lengthOfLongestSubstring(s))
 }
