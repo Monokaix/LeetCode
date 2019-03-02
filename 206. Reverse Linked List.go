3b3c5a1ac7dcb682c9afc0cabe552aeaa2625c55
Monokaix <2536818783@qq.com>
2019-03-02 11:20:07 +0800

    Use three pointer and recursively

--- 206. Reverse Linked List.go
+++ 206. Reverse Linked List.go
@@ -6,7 +6,7 @@ package main
  	Val int
     Next *ListNode
 }
-
+// normal solution
 func reverseList(head *ListNode) *ListNode {
 	pre := &ListNode{}
 	pre = nil
@@ -20,3 +20,14 @@ func reverseList(head *ListNode) *ListNode {
 	}
 	return pre
 }
+
+// recursively
+//func reverseList(head *ListNode) *ListNode {
+//	if head == nil || head.Next == nil{
+//		return head
+//	}
+//	newHead := reverseList(head.Next)
+//	head.Next.Next = head
+//	head.Next = nil
+//	return newHead
+//}
