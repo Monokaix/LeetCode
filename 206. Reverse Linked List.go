e28ebcf4fce4afdae67919e6b49f7a06319f46ab
Monokaix <2536818783@qq.com>
2019-03-03 13:41:34 +0800

    reverse with two methods

--- 206. Reverse Linked List.go
+++ 206. Reverse Linked List.go
@@ -1,45 +1,21 @@
 package main
 
 
-
- type ListNode struct {
- 	Val int
-    Next *ListNode
-}
 // normal solution
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 2a48fd708ab7a7f03944223346c8bf863e1d41f4
-//func reverseList(head *ListNode) *ListNode {
-//	pre := &ListNode{}
-//	pre = nil
-//	cur := head
-//	for cur != nil{
-//		next := cur.Next
-//
-//		cur.Next = pre //reverse
-//		pre = cur
-//		cur = next
-//	}
-//	return pre
-//}
-
-// recursively
-<<<<<<< HEAD
->>>>>>> 2a48fd708ab7a7f03944223346c8bf863e1d41f4
-=======
->>>>>>> 2a48fd708ab7a7f03944223346c8bf863e1d41f4
 func reverseList(head *ListNode) *ListNode {
-	if head == nil || head.Next == nil{
-		return head
+	pre := &ListNode{}
+	pre = nil
+	cur := head
+	for cur != nil{
+		next := cur.Next
+
+		cur.Next = pre //reverse
+		pre = cur
+		cur = next
 	}
-	newHead := reverseList(head.Next)
-	head.Next.Next = head
-	head.Next = nil
-	return newHead
+	return pre
 }
+
 
 // recursively
 //func reverseList(head *ListNode) *ListNode {
