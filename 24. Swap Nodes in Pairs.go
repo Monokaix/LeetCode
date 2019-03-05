64db914302e1b347bc8ec359fc29c7765a028e91
Monokaix <2536818783@qq.com>
2019-03-05 19:19:31 +0800

    use four pointers

---
+++ 24. Swap Nodes in Pairs.go
@@ -0,0 +1,25 @@
+package main
+
+// four pointers
+func swapPairs(head *ListNode) *ListNode {
+	dummyHead := &ListNode{}
+	dummyHead.Next = head
+
+	p := dummyHead
+	for p.Next != nil && p.Next.Next != nil{
+		node1 := p.Next
+		node2 := node1.Next
+		next := node2.Next
+		//swap
+
+		node2.Next = node1
+		node1.Next = next
+		p.Next = node2
+
+		// next traverse
+		p = node1
+	}
+
+	return dummyHead.Next
+
+}
