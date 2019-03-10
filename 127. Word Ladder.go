ece46a9b19f2f5de5af0a612b2085415a6ce7cbb
Monokaix <2536818783@qq.com>
2019-03-10 11:55:41 +0800

    graph bfs same as 279

---
+++ 127. Word Ladder.go
@@ -0,0 +1,38 @@
+package main
+
+import "container/list"
+
+func ladderLength(beginWord string, endWord string, wordList []string) int {
+	l := list.New()
+	type info struct {
+		word string
+		step int
+	}
+	l.PushBack(info{beginWord,0})
+	visited := make(map[string]bool)
+	visited[beginWord] = true
+	for l.Len() > 0{
+		word := l.Front().Value.(info).word
+		step := l.Front().Value.(info).step
+		l.Remove(l.Front())
+		// Find a word that differs by one letter in wordList
+		for i:= 0;i<len(wordList);i++{
+			different := 0
+			for j := 0;j<len(wordList[i]);j++{
+				if word[j] != wordList[i][j]{
+					different++
+				}
+			}
+			if different == 1{
+				if _,ok := visited[wordList[i]];!ok{
+					l.PushBack(info{wordList[i],step+1})
+					visited[wordList[i]] = true
+				}
+			}
+		}
+		if word == endWord{ //find the shortest path
+			return step+1
+		}
+	}
+	return 0
+}
