8e39953bce2045ad554dba3aa84c9f0283ad77da
Monokaix <2536818783@qq.com>
2019-03-11 15:23:34 +0800

    binary tree recursively same as 104

---
+++ 111. Minimum Depth of Binary Tree.go
@@ -0,0 +1,16 @@
+package main
+
+func minDepth(root *TreeNode) int {
+	if root == nil{
+		return 0
+	}
+	left := minDepth(root.Left)
+	right := minDepth(root.Right)
+	if left == 0 || right == 0{
+		return left+right+1
+	}
+	if left < right{
+		return left+1
+	}
+	return right+1
+}
