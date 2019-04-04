bc98af5f7480e62fb3d04a76b232fcbadb868079
Monokaix <2536818783@qq.com>
2019-04-04 14:00:09 +0800

    greedy

---
+++ 392. Is Subsequence.go
@@ -0,0 +1,15 @@
+package main
+
+func isSubsequence(s string, t string) bool {
+	i := 0
+	j := 0
+	for i < len(s) && j < len(t) {
+		if s[i] == t[j] {
+			i++
+			j++
+		} else {
+			j++
+		}
+	}
+	return i == len(s)
+}
