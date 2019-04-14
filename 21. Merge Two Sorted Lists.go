e13236c1309b1b7ecbef7cbef48e2637613a816c
Monokaix <2536818783@qq.com>
2019-04-14 13:40:49 +0800

    recursive and add comment

--- 21. Merge Two Sorted Lists.go
+++ 21. Merge Two Sorted Lists.go
@@ -4,7 +4,7 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 	l3 := &ListNode{}
 	cur := l3
 	for l1 != nil && l2 !=nil{
-		if l1.Val <= l2.Val{
+		if l1.Val <= l2.Val{ //这里比较m+n次
 			cur.Next = l1
 			cur = cur.Next
 			l1 = l1.Next
@@ -22,3 +22,6 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 
 	return l3.Next
 }
+// 时间复杂度：最坏O（m+n） m、n分别为两个链表长度 平均O（min（m，n））
+// 比如1，2，4和1，5比较
+// 总共比较次数为（1，1）（2，1）（2，4）（3，4）共四次比较，最后把4再插入链表共m+n次操作
--- 23. Merge k Sorted Lists.go
+++ 23. Merge k Sorted Lists.go
@@ -14,16 +14,19 @@ func mergeKLists(lists []*ListNode) *ListNode {
 	}
 	//拆分为两个链表递归调用
 	mid := n / 2
-	list1 := make([]*ListNode, 0)
-	list2 := make([]*ListNode, 0)
-	for i := 0; i < mid; i++ {
-		list1 = append(list1, lists[i])
-	}
-	for i := mid; i < n; i++ {
-		list2 = append(list2, lists[i])
-	}
-	l1 := mergeKLists(list1)
-	l2 := mergeKLists(list2)
+
+	l1 := mergeKLists(lists[:mid])
+	l2 := mergeKLists(lists[mid:])
 
 	return mergeTwoLists(l1, l2)
 }
+
+// 方法一：笨方法：k个链表，前两个合并，然后合并第3个再合并第4个共合并k-1次，第一次比较最坏比较次数O（m+n）,
+// 假设链表平均长度都为n，则为O（n+n），第二次为O（2n+n）,以此类推，相加得（n+n）+（2n+n）+（3n+n）+...
+// +(k-1)n+n = (1+2+3+...+k-1)n+(k-1)n = k(k+1)/2*n-n=O(k^2*n)
+// 方法二：k个链表，每个链表平均n个节点，两两合并，
+// 第一轮进行k/2次，每次处理2n个数字
+// 第二轮进行k/4次，每次处理4n个数字，以此类推
+// 可以得到最后一轮共k/(2^logk)，处理2^logk*n个值
+// 计算得到每一轮操作次数为kn，共进行logk次，所以时间复杂度为O(knlogk)
+// 每次两两合并，合并完再两两合并最后得到一个完整得链表
