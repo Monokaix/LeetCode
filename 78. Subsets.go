7952e40492f4b71f311a37d400ea18a1ce1d8c72
Monokaix <2536818783@qq.com>
2019-05-18 20:38:20 +0800

    combination problem with backtracking

--- 78. Subsets.go
+++ 78. Subsets.go
@@ -12,7 +12,7 @@ func subsets(nums []int) [][]int {
 func combination5(nums []int, cur []int, res *[][]int, begin int) {
 	temp := make([]int, len(cur))
 	copy(temp, cur)
-	*res = append(*res, temp)
+	*res = append(*res, temp) //这里第一次加入的是一个空集
 	for i := begin; i < len(nums); i++ {
 		cur = append(cur, nums[i])
 		combination5(nums, cur, res, i+1) //i+1，因为不可以包括自身，对比problem39
