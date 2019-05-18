82f29701e674ef941736bc36ddb33e7e74e6645b
Monokaix <2536818783@qq.com>
2019-05-18 21:54:46 +0800

    combination problem with backtracking

--- 90. Subsets II.go
+++ 90. Subsets II.go
@@ -17,14 +17,13 @@ func combination6(nums []int, cur []int, res *[][]int, begin int) {
 	copy(temp, cur)
 	*res = append(*res, temp)
 	for i := begin; i < len(nums); i++ {
-		if i > begin && nums[i] == nums[i-1]{
-			continue
-		}
+		if i > begin && nums[i] == nums[i-1] { //排完序后，从第二个元素开始，若他与前一个元素相等，则不加入当前元素
+			continue                           //i大于begin，如本例中begin=0，i，表示在第一轮递归条件下（因为begin=0）
+		}                                      //后续的递归已经有重复元素，则跳过后续轮的递归，也就是说回溯后的第二个1越过了
 		cur = append(cur, nums[i])
 		combination6(nums, cur, res, i+1) //i+1，因为不可以包括自身，对比problem39
 		cur = cur[:len(cur)-1]
 	}
-	return
 }
 func main() {
 	nums := []int{1, 1}
