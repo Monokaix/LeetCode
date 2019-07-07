c21dac96a00691f5b4d407ca84769d4a76e97f64
Monokai <2536818783@qq.com>
2019-07-07 15:25:35 +0800

    combination problem with backtracking

--- 77. Combinations.go
+++ 77. Combinations.go
@@ -15,7 +15,7 @@ func combination(n int, k int, start int, cur []int, res *[][]int) {
 		return
 	}
 
-	for i := start; i <= n-(k-len(cur))+1; i++ {
+	for i := start; i <= n-(k-len(cur))+1; i++ { //这里i的取值上限这么定义是因为：每次开始的位置不能重复，即每个数字不能重复使用
 		cur = append(cur, i)
 		combination(n, k, i+1, cur, res) //i+1,因为不能包括自身，对比problem39
 		cur = cur[:len(cur)-1]
