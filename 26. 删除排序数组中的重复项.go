6d409dc0527cdd433eb421883c723f46f7b0232c
Monokai <2536818783@qq.com>
2019-07-29 14:42:25 +0800

    array and same as problem 83

---
+++ 26. 删除排序数组中的重复项.go
@@ -0,0 +1,18 @@
+package main
+
+//i指针一直往前移动,j指针指向当前应该被覆盖的位置
+//i指向的值和前一个值一样,则i继续滑动
+//遇到和前一个值不一样的时候,i覆盖掉j
+func removeDuplicates(nums []int) int {
+	num := len(nums)
+	j := 1
+	for i := 1; i < len(nums); i++ {
+		if nums[i] == nums[i-1] {
+			num-- //i指针继续往前移动
+		} else {
+			nums[j] = nums[i] //直接替换
+			j++               //同时j向前移动
+		}
+	}
+	return num
+}
