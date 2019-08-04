b5dbb516bbbdacd936d70eb24afcaf16c090e7de
Monokai <2536818783@qq.com>
2019-08-04 22:05:29 +0800

    dp and mancher

--- 5. Longest Palindromic Substring.go
+++ 5. Longest Palindromic Substring.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"math"
+)
+
 //方法一：暴力循环，因为前两重循环是找所有子串，第三重循环判断回文
 //方法二：dp，其实思路和方法一一样，只不过判断回文的时候用dp记录了，所以复杂度收益O(n)，dp[i][j]
 //记录从i到j是否为回文，若dp[i+1][j-1]是回文，则str[i]=str[j]时dp[i][j]也是回文，利用这一特性求解
@@ -35,3 +39,49 @@ func longestPalindrome(s string) string {
 	}
 	return s[start : maxLen+start]
 }
+//方法二
+//马拉车算法分为两大部分：
+//1、当前节点i在右边界mx外，直接暴力扩散
+//2、当前节点i在右边界内：
+//（1）i的对称点i在最大左右边界L内，则i的回文串长度为i的回文串长度
+//（2）i的对称点i在最大左右边界L外，则i的回文串长度为到R的距离。
+//（3）i的对称点i在最大左右边界L上，则i的回文串要计算R之后的那部分是否是回文串
+func Manacher(s string) string {
+	if s == "" {
+		return ""
+	}
+	str := "$#"
+	for i := 0; i < len(s); i++ {
+		str += string(s[i])
+		str += "#"
+	}
+	str += "@" //放置越界,因为不加的话第一个和最后一个都是#,会重复判断
+	p := make([]int, len(str))
+	id := 0 //最大回文半径的中心点
+	mx := 0 //右边界
+	resCenter := 0
+	resLen := 0
+	for i := 1; i < len(str)-1; i++ {
+		if mx > i { //当前i在右边界内
+			p[i] = int(math.Min(float64(p[2*id-i]), float64(mx-i))) //p[2*id-i]表示关于id对称位置处的回文半径
+		} else {
+			p[i] = 1
+		}
+		for i-p[i] >= 0 && i+p[i] < len(str)-1 && str[i+p[i]] == str[i-p[i]] { //i位置还可以继续扩列 对应i在右边界mx外和i的对称点i在最大左右边界L上时的暴力扩
+			p[i]++
+		}
+		if i+p[i] > mx { //有更大的右边界
+			id = i
+			mx = i + p[i]
+		}
+		if p[i] > resLen { //当前位置i的回文半径更长
+			resLen = p[i]
+			resCenter = i
+		}
+	}
+	//(resCenter-resLen)/2为起始索引,resLen-1为长度
+	return s[(resCenter-resLen)/2 : (resCenter-resLen)/2+resLen-1]
+
+}
+
+
