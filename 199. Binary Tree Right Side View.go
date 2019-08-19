e7413d6af940c9d38e9c777920c6d5ea0bf66385
Monokai <2536818783@qq.com>
2019-08-19 15:31:42 +0800

    binary tree level oreder and youhua

--- 199. Binary Tree Right Side View.go
+++ 199. Binary Tree Right Side View.go
@@ -2,38 +2,64 @@ package main
 
 import "container/list"
 
+//func rightSideView(root *TreeNode) []int {
+//	res := [][]int{}
+//	slice := []int{}
+//	if root == nil {
+//		return nil
+//	}
+//	l := list.New()
+//	type info struct {
+//		node  *TreeNode
+//		level int
+//	}
+//	l.PushBack(info{root, 0})
+//	for l.Len() > 0 {
+//		node := l.Front().Value.(info).node
+//		level := l.Front().Value.(info).level
+//		l.Remove(l.Front())
+//		// new level
+//		if level >= len(res) {
+//			res = append(res, []int{})
+//		}
+//		res[level] = append(res[level], node.Val)
+//		// left and right node to queue
+//		if node.Left != nil {
+//			l.PushBack(info{node.Left, level + 1})
+//		}
+//		if node.Right != nil {
+//			l.PushBack(info{node.Right, level + 1})
+//		}
+//	}
+//	// select the last value
+//	for i := 0;i<len(res);i++{
+//		slice = append(slice,res[i][len(res[i])-1])
+//	}
+//	return slice
+//}
+//类似于637号问题
 func rightSideView(root *TreeNode) []int {
-	res := [][]int{}
-	slice := []int{}
 	if root == nil {
 		return nil
 	}
+	res := make([]int, 0)
 	l := list.New()
-	type info struct {
-		node  *TreeNode
-		level int
-	}
-	l.PushBack(info{root, 0})
+	l.PushBack(root) //首先放入根节点
 	for l.Len() > 0 {
-		node := l.Front().Value.(info).node
-		level := l.Front().Value.(info).level
-		l.Remove(l.Front())
-		// new level
-		if level >= len(res) {
-			res = append(res, []int{})
+		size := l.Len() //这个size记录的是当前层的节点个数
+		tmp := 0
+		for i := 0; i < size; i++ {
+			node := l.Front().Value.(*TreeNode)
+			l.Remove(l.Front())
+			tmp = node.Val
+			if node.Left != nil {
+				l.PushBack(node.Left)
+			}
+			if node.Right != nil {
+				l.PushBack(node.Right)
+			}
 		}
-		res[level] = append(res[level], node.Val)
-		// left and right node to queue
-		if node.Left != nil {
-			l.PushBack(info{node.Left, level + 1})
-		}
-		if node.Right != nil {
-			l.PushBack(info{node.Right, level + 1})
-		}
+		res = append(res, tmp)
 	}
-	// select the last value
-	for i := 0;i<len(res);i++{
-		slice = append(slice,res[i][len(res[i])-1])
-	}
-	return slice
+	return res
 }
