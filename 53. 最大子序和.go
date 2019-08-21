110b25e7d11a79aaf342acae49b1b289ae5b8d5c
Monokai <2536818783@qq.com>
2019-08-21 21:07:07 +0800

    array and dp

--- 53. 最大子序和.go
+++ 53. 最大子序和.go
@@ -1,20 +1,20 @@
 package main
 
-import "math"
-
-//思路：dp 注意题目要求连续！！！
-//因为要求是连续，所以用dp[i]表示以第i个数字结尾所能表示的最大连续和，最后再比较数组得到最大值
-//当dp[i-1]<0时，则dp[i]即为nums[i]，否则dp[i]即为dp[i-1]+nums[i]
-func maxSubArray(nums []int) int {
-	n := len(nums)
-	dp := make([]int, n)
-	dp[0] = nums[0]
-	max := dp[0]
-	for i := 1; i < n; i++ {
-		dp[i] = int(math.Max(float64(nums[i]), float64(dp[i-1]+nums[i])))
-		if dp[i] > max {
-			max = dp[i]
+//状态转移方程 Sum[i] = max(a[i]+Sum[i-1],a[i]),curSum[i]表示以i结尾的求出的最大值
+//表示最大值要么为当前已求得的curSum加上当前元素a[i]后的值，要么是a[i],因为负数加个数永远不会有这个数本身大(从a[i]开始重新累加,curSum清零)
+func maxArrSeq(nums []int) int {
+	maxSum := nums[0]
+	curSum := 0
+	for i := 0; i < len(nums); i++ {
+		if curSum > 0 {
+			curSum += nums[i] //a[i]+Sum[i-1]
+		} else {
+			curSum = nums[i] //a[i]
+		}
+		//找出最大值
+		if curSum > maxSum {
+			maxSum = curSum
 		}
 	}
-	return max
+	return maxSum
 }
