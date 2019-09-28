4ac34c877f8d6757d191b63100f91aa9a3976d68
Monokai <2536818783@qq.com>
2019-09-28 16:54:11 +0800

    dp and LIS

--- 300. Longest Increasing Subsequence.go
+++ 300. Longest Increasing Subsequence.go
@@ -6,20 +6,14 @@ func lengthOfLIS(nums []int) int {
 		return 0
 	}
 	dp := make([]int, n)
-	for i := 0; i < n; i++ {
-		dp[i] = 1 //初始每个元素最长子序列都为1
-	}
+	res := 0
 	//dp[i]记录以第i个位置结束的最长子序列长度
 	for i := 1; i < n; i++ {
 		for j := 0; j < i; j++ {
 			if nums[j] < nums[i] {
 				dp[i] = Max(dp[i], 1+dp[j])
 			}
-
 		}
-	}
-	res := 1
-	for i := 0; i < n; i++ {
 		if res < dp[i] {
 			res = dp[i]
 		}
