88c93cf8642e9687eb30a4d7714ea02c8e652f0f
Monokai <2536818783@qq.com>
2019-10-22 16:39:38 +0800

    binary recursive and add new solve

--- 98. Validate Binary Search Tree.go
+++ 98. Validate Binary Search Tree.go
@@ -1,5 +1,7 @@
 package main
 
+import "math"
+
 func isValidBST(root *TreeNode) bool {
 	if root == nil {
 		return true
@@ -26,4 +28,22 @@ func dfs_right(root *TreeNode, root_val int) bool {
 		return false
 	}
 	return dfs_right(root.Left, root_val) && dfs_right(root.Right, root_val)
+}
+
+//方法二 cmp是递增的,因为搜索树要满足中序序列递增
+func isValidBST2(root *TreeNode) bool {
+	cmp := math.MinInt64
+	return isValidBST3(root, &cmp)
+}
+func isValidBST3(root *TreeNode, cmp *int) bool {
+	if root == nil {
+		return true
+	}
+	if isValidBST3(root.Left, cmp) {
+		if *cmp < root.Val {
+			*cmp = root.Val
+			return isValidBST3(root.Right, cmp)
+		}
+	}
+	return false
 }
