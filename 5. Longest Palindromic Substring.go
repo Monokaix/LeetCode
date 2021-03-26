28ac4d6e0f58e659fe622be0c462b40f86d8d7e4
xuzheng chang <changxuzheng@huawei.com>
2021-03-26 17:32:32 +0800

    add another method

--- 5. Longest Palindromic Substring.go
+++ 5. Longest Palindromic Substring.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math"
 )
 
@@ -39,6 +40,7 @@ func longestPalindrome(s string) string {
 	}
 	return s[start : maxLen+start]
 }
+
 //方法二
 //马拉车算法分为两大部分：
 //1、当前节点i在右边界mx外，直接暴力扩散
@@ -84,4 +86,45 @@ func Manacher(s string) string {
 
 }
 
+// 方法三：简单思路
+// 以每个字符为中心向左右扩，求得以每个字符为中心的最大回文子串，最后在外层循环找出整个字符串的最大回文子串
+// 由于要考虑奇数和偶数，所以要以一个字符和两个字符分别来找出当前最大回文子串。复杂度O(n)
+func longestPalindrome3(s string) string {
+	if len(s) < 2 {
+		return s
+	}
 
+	start := 0
+	end := 0
+	for i := 0; i < len(s); i++ {
+		len1 := expandAroundCenter(s, i, i)
+		len2 := expandAroundCenter(s, i, i+1)
+		length := max2(len1, len2)
+		if end-start < length {
+			start = i - (length-1)/2
+			end = i + length/2
+		}
+	}
+
+	return s[start : end+1]
+}
+
+func expandAroundCenter(s string, left, right int) int {
+	for left >= 0 && right < len(s) && s[left] == s[right] {
+		left--
+		right++
+	}
+	return right - left - 1
+}
+
+func max2(i, j int) int {
+	if i > j {
+		return i
+	}
+	return j
+}
+
+func main() {
+	s := "cbbd"
+	fmt.Println(longestPalindrome3(s))
+}
