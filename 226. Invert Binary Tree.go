3004c198d879ba48c7089e9ce7fbbbad25b8c58f
xuzheng chang <changxuzheng@huawei.com>
2021-03-31 20:41:14 +0800

    binary and recursive

--- 226. Invert Binary Tree.go
+++ 226. Invert Binary Tree.go
@@ -1,11 +1,27 @@
 package main
 
+//func invertTree(root *TreeNode) *TreeNode {
+//	if root == nil{
+//		return nil
+//	}
+//	root.Left,root.Right= root.Right,root.Left
+//	invertTree(root.Left)
+//	invertTree(root.Right)
+//	return root
+//}
+
+// 方法二
 func invertTree(root *TreeNode) *TreeNode {
-	if root == nil{
+	if root == nil {
 		return nil
 	}
-	root.Left,root.Right= root.Right,root.Left
-	invertTree(root.Left)
-	invertTree(root.Right)
+
+	// 分别反转左右子树
+	left := invertTree(root.Left)
+	right := invertTree(root.Right)
+	// 然后左右互换两个子树
+	root.Left = right
+	root.Right = left
+
 	return root
 }
