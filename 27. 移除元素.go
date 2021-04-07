dc1084d024015cdf1e8cfb3cd6cd1b3984918aae
xuzheng chang <changxuzheng@huawei.com>
2021-04-07 19:31:49 +0800

    slide window

---
+++ 27. 移除元素.go
@@ -0,0 +1,13 @@
+package main
+
+func removeElement(nums []int, val int) int {
+	i := 0
+	for j := 0; j < len(nums); j++ {
+		if nums[j] != val {
+			nums[i] = nums[j]
+			i++
+		}
+	}
+
+	return i + 1
+}
