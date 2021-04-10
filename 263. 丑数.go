1ca9e4aa5135a06ffb3a93ea6dd05a5168c63eea
Monokai <2536818783@qq.com>
2021-04-10 16:24:08 +0800

    dp

---
+++ 263. 丑数.go
@@ -0,0 +1,19 @@
+package main
+
+func isUgly(n int) bool {
+	if n < 1 {
+		return false
+	}
+	for n > 1 {
+		if n%2 == 0 {
+			n /= 2
+		} else if n%3 == 0 {
+			n /= 3
+		} else if n%5 == 0 {
+			n /= 5
+		} else {
+			return false
+		}
+	}
+	return true
+}
---
+++ 264. 丑数 II.go
@@ -0,0 +1,23 @@
+package main
+
+func nthUglyNumber(n int) int {
+	dp := make([]int, n+1)
+	dp[1] = 1
+
+	p2, p3, p5 := 1, 1, 1
+	for i := 2; i <= n; i++ {
+		x2, x3, x5 := dp[p2]*2, dp[p3]*3, dp[p5]*5
+		dp[i] = min(min(x2, x3), x5)
+		if dp[i] == x2 {
+			p2++
+		}
+		if dp[i] == x3 {
+			p3++
+		}
+		if dp[i] == x5 {
+			p5++
+		}
+	}
+
+	return dp[n]
+}
