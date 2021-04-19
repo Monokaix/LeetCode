33f6f91b1da94daa1cd762b4d62785074190e3cd
xuzheng chang <changxuzheng@huawei.com>
2021-04-19 09:40:13 +0800

    array and add method

--- 27. 移除元素.go
+++ 27. 移除元素.go
@@ -1,13 +1,34 @@
 package main
 
+//func removeElement(nums []int, val int) int {
+//	i := 0
+//	for j := 0; j < len(nums); j++ {
+//		if nums[j] != val {
+//			nums[i] = nums[j]
+//			i++
+//		}
+//	}
+//
+//	return i + 1
+//}
+
+// 方法二
 func removeElement(nums []int, val int) int {
 	i := 0
-	for j := 0; j < len(nums); j++ {
-		if nums[j] != val {
-			nums[i] = nums[j]
+	n := len(nums)
+	j := n - 1
+	for i <= j {
+		for j >= 0 && nums[j] == val {
+			j--
+		}
+		for i < n && nums[i] != val {
 			i++
+		}
+
+		if i < n && j >= 0 && i <= j {
+			nums[i], nums[j] = nums[j], nums[i]
 		}
 	}
 
-	return i + 1
+	return i
 }
