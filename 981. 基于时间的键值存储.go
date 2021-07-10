f13255293529a5ec9144e5a941b088c22d397a9d
xuzheng chang <changxuzheng@huawei.com>
2021-07-10 14:38:51 +0800

    binary search

---
+++ 981. 基于时间的键值存储.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"sort"
+	"strconv"
+)
+
+type TimeMap struct {
+	key2TimeStamp   map[string][]int
+	timeStamp2Value map[string]string
+}
+
+/** Initialize your data structure here. */
+func Constructor() TimeMap {
+	return TimeMap{
+		key2TimeStamp:   make(map[string][]int),
+		timeStamp2Value: make(map[string]string),
+	}
+}
+
+func (this *TimeMap) Set(key string, value string, timestamp int) {
+	this.timeStamp2Value[key+strconv.Itoa(timestamp)] = value
+	arr := this.key2TimeStamp[key]
+	arr = append(arr, timestamp)
+	this.key2TimeStamp[key] = arr
+}
+
+func (this *TimeMap) Get(key string, timestamp int) string {
+	arr := this.key2TimeStamp[key]
+	if len(arr) == 0 {
+		return ""
+	}
+	index := sort.SearchInts(arr, timestamp)
+	if index == len(arr) {
+		return this.timeStamp2Value[key+strconv.Itoa(arr[index-1])]
+	}
+	if arr[index] == timestamp {
+		return this.timeStamp2Value[key+strconv.Itoa(timestamp)]
+	}
+	if index > 0 {
+		timestamp = arr[index-1]
+		return this.timeStamp2Value[key+strconv.Itoa(timestamp)]
+	}
+	return ""
+	sort.Search()
+}
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Set(key,value,timestamp);
+ * param_2 := obj.Get(key,timestamp);
+ */
