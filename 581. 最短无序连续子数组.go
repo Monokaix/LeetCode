6932ba29e8c4f057c97491df044bb66eaaaa4619
xuzheng chang <changxuzheng@huawei.com>
2021-08-03 18:50:26 +0800

    array

---
+++ 581. 最短无序连续子数组.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func findUnsortedSubarray(nums []int) int {
+	l, r := -1, -1
+	n := len(nums)
+	min := math.MaxInt32
+	max := math.MinInt32
+	for i := 0; i < n; i++ {
+		if nums[i] < max {
+			r = i
+		} else {
+			max = nums[i]
+		}
+	}
+	for i := n - 1; i >= 0; i-- {
+		if nums[i] > min {
+			l = i
+		} else {
+			min = nums[i]
+		}
+	}
+	fmt.Println(l, r)
+
+	if l == r {
+		return 0
+	}
+	return r - l + 1
+}
+
+func main() {
+	nums := []int{2, 6, 4, 8, 10, 9, 15}
+	findUnsortedSubarray(nums)
+}
